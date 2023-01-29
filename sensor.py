22362adaea1fff654fe27211a1b9cf11d9c31934
GaetanRAYNAUD <gaetanraynaud@outlook.com>
2023-01-29 12:59:08 +0100

    Finish working integration

--- sensor.py
+++ sensor.py
@@ -1,79 +1,138 @@
 import datetime
+import logging
 
+import homeassistant.helpers.config_validation as cv
 import requests
-
+import voluptuous
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
+from homeassistant.const import UnitOfTime
 
-config = {
-    'apiKey': '',
-    'start': '43082',
-    # Optional
-    'end': '41194',
-    # Optional
-    'line': 'C01739'
-    # 'line': 'C01742'
-}
+DOMAIN = "idfm"
 
-apiKey = config['apiKey']
-start = config['start']
-end = config['end'] if 'end' in config else None
-line = config['line'] if 'line' in config else None
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        voluptuous.Required('name'): cv.string,
+        voluptuous.Required('api_key'): cv.string,
+        voluptuous.Required('start'): cv.string,
+        voluptuous.Optional('end'): [cv.string],
+        voluptuous.Optional('line'): cv.string
+    }
+)
 
-response = requests.get('https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring',
-                        params={'MonitoringRef': 'STIF:StopPoint:Q:' + start + ':'}, headers={'apiKey': apiKey})
-lineRef = 'STIF:Line::' + line + ':' if line is not None else None
-endRef = 'STIF:StopPoint:Q:' + end + ':' if end is not None else None
+log = logging.getLogger('idfm')
 
-if response.status_code == 200:
-    body = response.json()
 
-    if 'Siri' in body and 'ServiceDelivery' in body['Siri'] \
-            and 'StopMonitoringDelivery' in body['Siri']['ServiceDelivery'] \
-            and len(body['Siri']['ServiceDelivery']['StopMonitoringDelivery']) > 0 \
-            and 'MonitoredStopVisit' in body['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0] \
-            and len(body['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit']) > 0:
-        visits = body['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit']
-        results = []
-        startName = None
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    end = config['end'] if 'end' in config else None
+    line_ref = config['line'] if 'line' in config else None
 
-        for visit in visits:
-            if 'MonitoredVehicleJourney' in visit:
-                journey = visit['MonitoredVehicleJourney']
+    add_entities([LineSensor(config['name'], config['api_key'], config['start'], end, line_ref)])
 
-                if 'LineRef' in journey and 'value' in journey['LineRef']:
-                    lineValue = journey['LineRef']['value']
 
-                    if lineRef is None or lineValue == lineRef:
-                        if endRef is None or ('DestinationRef' in journey and 'value' in journey['DestinationRef']
-                                              and endRef == journey['DestinationRef']['value']):
-                            if 'MonitoredCall' in journey:
-                                call = journey['MonitoredCall']
+class LineSensor(SensorEntity):
+    _attr_icon = 'mdi:train'
+    _attr_native_unit_of_measurement = UnitOfTime.MINUTES
 
-                                if 'DepartureStatus' in call and 'cancelled' != call['DepartureStatus']:
-                                    if 'ExpectedArrivalTime' in call:
-                                        arrival = datetime.datetime.fromisoformat(call['ExpectedArrivalTime'])
+    def __init__(self, name, api_key, start, end, line):
+        self._name = name
+        self.data = []
+        self.api_key = api_key
+        self.start = start
+        self.end = end
+        self.line = line
+        self.minutes = None
+        self.last_update = None
+        self.start_name = None
 
-                                        if arrival > datetime.datetime.now().astimezone():
-                                            result = {
-                                                'line': lineValue.replace('STIF:Line::', '')[:-1],
-                                                'time': arrival.isoformat()
-                                            }
+    def update(self):
+        response = requests.get('https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring',
+                                params={'MonitoringRef': 'STIF:StopPoint:Q:' + self.start + ':'},
+                                headers={'apiKey': self.api_key})
+        lineRef = 'STIF:Line::' + self.line + ':' if self.line is not None else None
+        endRef = None
 
-                                            if 'ArrivalPlatformName' in call and 'value' in call['ArrivalPlatformName']:
-                                                platform = call['ArrivalPlatformName']['value']
-                                                result['platform'] = platform
+        if self.end is not None:
+            endRef = []
+            for e in self.end:
+                endRef.append('STIF:StopPoint:Q:' + e + ':')
 
-                                            if 'DestinationName' in journey and len(journey['DestinationName']) > 0 \
-                                                    and 'value' in journey['DestinationName'][0]:
-                                                result['end'] = journey['DestinationName'][0]['value']
+        if response.status_code == 200:
+            body = response.json()
+            self.data = []
+            self.minutes = None
 
-                                            if startName is None and 'StopPointName' in call \
-                                                    and len(call['StopPointName']) > 0 \
-                                                    and 'value' in call['StopPointName'][0]:
-                                                startName = call['StopPointName'][0]['value']
+            if 'Siri' in body and 'ServiceDelivery' in body['Siri'] \
+                    and 'StopMonitoringDelivery' in body['Siri']['ServiceDelivery'] \
+                    and len(body['Siri']['ServiceDelivery']['StopMonitoringDelivery']) > 0 \
+                    and 'MonitoredStopVisit' in body['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0] \
+                    and len(body['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit']) > 0:
+                visits = body['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit']
 
-                                            results.append(result)
+                for visit in visits:
+                    if 'MonitoredVehicleJourney' in visit:
+                        journey = visit['MonitoredVehicleJourney']
 
-        print(startName)
-        for result in results:
-            print(result)
+                        if 'LineRef' in journey and 'value' in journey['LineRef']:
+                            lineValue = journey['LineRef']['value']
+
+                            if lineRef is None or lineValue == lineRef:
+                                if endRef is None or (
+                                        'DestinationRef' in journey and 'value' in journey['DestinationRef']
+                                        and journey['DestinationRef']['value'] in endRef):
+                                    if 'MonitoredCall' in journey:
+                                        call = journey['MonitoredCall']
+
+                                        if 'DepartureStatus' in call and 'cancelled' != call['DepartureStatus']:
+                                            if 'ExpectedArrivalTime' in call:
+                                                arrival = datetime.datetime \
+                                                    .strptime(call['ExpectedArrivalTime'],
+                                                              '%Y-%m-%dT%H:%M:%S.%fZ').astimezone()
+
+                                                if arrival > datetime.datetime.now(datetime.timezone.utc):
+                                                    result = {
+                                                        'line': lineValue.replace('STIF:Line::', '')[:-1],
+                                                        'time': arrival
+                                                    }
+
+                                                    if 'ArrivalPlatformName' in call \
+                                                            and 'value' in call['ArrivalPlatformName']:
+                                                        platform = call['ArrivalPlatformName']['value']
+                                                        result['platform'] = platform
+
+                                                    if 'DestinationName' in journey \
+                                                            and len(journey['DestinationName']) > 0 \
+                                                            and 'value' in journey['DestinationName'][0]:
+                                                        result['end'] = journey['DestinationName'][0]['value']
+
+                                                    if self.start_name is None and 'StopPointName' in call \
+                                                            and len(call['StopPointName']) > 0 \
+                                                            and 'value' in call['StopPointName'][0]:
+                                                        self.start_name = call['StopPointName'][0]['value']
+
+                                                    self.data.append(result)
+
+            self.data = sorted(self.data, key=lambda d: d['time'])
+            self.last_update = datetime.datetime.now()
+
+            if self.data is not None and len(self.data) > 0:
+                self.minutes = round(
+                    (self.data[0]['time'] - datetime.datetime.now(datetime.timezone.utc)).total_seconds() / 60)
+        else:
+            log.error('Error while calling IDFM api:', response.reason)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def native_value(self):
+        return self.minutes
+
+    @property
+    def extra_state_attributes(self):
+        return {
+            'last_update': self.last_update,
+            'start_name': self.start_name,
+            'minutes': self.minutes,
+            'trains': self.data
+        }
